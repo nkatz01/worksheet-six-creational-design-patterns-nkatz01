ad5f86c29802db7ff58142ce64fae149299f282c
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-03-02 21:20:04 +0000

    copied idea from helloworld with factory

--- Worksheet/AbstractFactory/ParserFactoryProducer.cs
+++ Worksheet/AbstractFactory/ParserFactoryProducer.cs
@@ -1,10 +1,26 @@
+using System.Configuration;
+using System;
+
 namespace QuestionOne
 {
     public static class ParserFactoryProducer
     {
+
+        internal static IAbstractParserFactory specificCompanyFactory { get; }
+
+       static ParserFactoryProducer(){
+        
+         var appSettings = ConfigurationManager.AppSettings;
+            var companyFactory = appSettings[nyFactory];
+            var t = Type.GetType(companyFactory);
+             specificCompanyFactory = Activator.CreateInstance(t) as IAbstractParserFactory;
+            return specificCompanyFactory;
+        }
+        
+        
         public static IAbstractParserFactory GetFactory(string nyFactory)
         {
-            throw new System.NotImplementedException();
+           
         }
     }
 }
