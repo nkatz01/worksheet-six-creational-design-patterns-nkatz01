78c0d9cad37e7bee79dd65e671648f22465c924f
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-03-02 20:39:50 +0000

    started working on implementing abstractFactory

---
+++ Worksheet/AbstractFactory/ConcreteParserFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionOne
+{
+    class NYCFactory : IAbstractParserFactory
+    {
+
+        public IXMLParser GetParserInstance(string parserType)
+        {
+
+            return new NYCFeedbackXMLParser();
+        }
+    }
+}
