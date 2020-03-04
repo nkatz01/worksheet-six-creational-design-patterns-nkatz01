4e8d4c08f546da7a64d15b847ec6a74c20b0196e
=nkatz01 <=>
2020-03-04 21:42:37 +0000

    partly implemented singleton

--- Worksheet/Singleton/LazySingleton.cs
+++ Worksheet/Singleton/LazySingleton.cs
@@ -2,6 +2,35 @@ namespace QuestionFive
 {
     public class LazySingleton
     {
-        
+
+        private static LazySingleton _LSInstance { get; set; } 
+
+        public string SomeString { get; set; }
+        private LazySingleton()
+        {
+
+        }
+
+        private LazySingleton(string SomeString)
+        {
+            this.SomeString = SomeString;
+        }
+        //Not thread safe but only instanciates it on demend thus avoiding instanciation everytime the class is loaded. 
+        //The reason I may choose to do this is because I have full control over instantiation and can do tasks beforehand and use a non-defualt constroctor.
+        public  static LazySingleton GetInstnace() {  
+            
+       
+
+            if (_LSInstance == null)
+            
+                _LSInstance = new LazySingleton("What's up");
+
+            
+
+            return _LSInstance;
+             
+        }
     }
+
+ 
 }
--- Worksheet/Singleton/TestSingletons.cs
+++ Worksheet/Singleton/TestSingletons.cs
@@ -6,7 +6,29 @@ namespace QuestionFive
     {
         public static void Main(string[] args)
         {
-            // do something here
+            LazySingleton LS1 = LazySingleton.GetInstnace();
+            LazySingleton LS2 = LazySingleton.GetInstnace();
+
+            Console.WriteLine(LS1 == LS2);
+           // Console.WriteLine(LS1.SomeString);
+
+            StaticSingleton S1 = StaticSingleton.GetInstnace();
+            StaticSingleton S2 = StaticSingleton.GetInstnace();
+
+
+            Console.WriteLine( S1 ==  S2);
+
+            StaticSingleton S3 = (StaticSingleton)S2.Clone();
+            Console.WriteLine(S3 == S2);
+
+            //LazySingleton ReflectedSS1 = (LazySingleton) Activator.CreateInstance(typeof(LazySingleton));
+            //LazySingleton ReflectedSS2 = (LazySingleton)Activator.CreateInstance(typeof(LazySingleton));
+            //LazySingleton ReflectedSS1 = (LazySingleton) typeof(LazySingleton).GetInstnace();
+            //LazySingleton ReflectedSS2 = (LazySingleton) typeof(LazySingleton).GetInstnace();
+
+            //Console.WriteLine(ReflectedSS1 == ReflectedSS2);
+
+
         }
     }
 }
