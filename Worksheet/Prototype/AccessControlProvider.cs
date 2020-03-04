9aa9dda9a685e5740e0c9a55b328467e7d448a37
=nkatz01 <=>
2020-03-04 11:21:56 +0000

    implemented some of accesscontrolprovider

--- Worksheet/Prototype/AccessControlProvider.cs
+++ Worksheet/Prototype/AccessControlProvider.cs
@@ -1,12 +1,19 @@
 using System;
-
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using static QuestionFour.AccessControl;
 namespace QuestionFour
 {
     public static class AccessControlProvider
     {
+        private static IEnumerable<KeyValuePair<string, AccessControl>> _AccessCtrlPerUserLevelMap;
         public static AccessControl GetAccessControlObject(string controlLevel) {
-            // is a "string" the best way to implement this?
-            throw new NotImplementedException();
+          
+            var ac = (AccessControl)_AccessCtrlPerUserLevelMap.Where(i => i.Key == controlLevel).Select(i => i.Value);
+              return ac;
+           
+            
         }
     }
 }
